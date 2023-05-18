6c63e69ad171a23ba644ed443ecd8aa567c931e7
AntonioLVieira <antonio.allvieira@gmail.com>
2023-05-18 17:12:06 +0100

    Ex.3 - Testagem de coleções

---
+++ StringCollections/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Olá");
+            list.Add("Adeus");
+            list.Add("Oi");
+
+            Console.WriteLine("Lista: ");
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            Stack<string> stack = new Stack<string>();
+            stack.Push("gato");
+            stack.Push("sapo");
+            stack.Push("águia");
+
+            Console.WriteLine("Stack: ");
+            while (stack.Count > 0)
+            {
+                string item = stack.Pop();
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("salamandra");
+            queue.Enqueue("gaivota");
+            queue.Enqueue("raposa");
+
+            Console.WriteLine("Queue: ");
+            while (queue.Count > 0)
+            {
+                string item = queue.Dequeue();
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            HashSet<string> hashSet = new HashSet<string>();
+            hashSet.Add("elefante");
+            hashSet.Add("girafa");
+            hashSet.Add("golfinho");
+
+            Console.WriteLine("HashSet: ");
+            foreach (string item in hashSet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
